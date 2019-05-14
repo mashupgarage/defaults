47faa934aaf3e5c7ce928dc90f578c0cae61acf8
Jekri Preclaro Orlina <jekku@users.noreply.github.com>
2019-05-14 16:13:21 +0800

    Shortens line length

--- mix/.formatter.exs
+++ mix/.formatter.exs
@@ -2,5 +2,5 @@
   import_deps: [:ecto, :phoenix],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test,priv}/**/*.{ex,exs}"],
   subdirectories: ["priv/*/migrations"],
-  line_length: 98
+  line_length: 80
 ]
